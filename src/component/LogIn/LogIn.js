23c80d7763208ef08e9cc8d10ee3d84d546beb33
Md Hasanul Karim <hasanlink71@yahoo.com>
2022-10-14 20:44:33 +0600

    a log in is working

--- src/component/LogIn/LogIn.js
+++ src/component/LogIn/LogIn.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import app from './firebase.init';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import { Result } from 'postcss';
+
+const auth = getAuth(app)
 
 const LogIn = () => {
+  const [user, setUser] = useState({})
+  const handleGoogleSignOut = () => {
+    signOut(auth);
+    setUser({});
+  }
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, provider)
+      .then(result => {
+        const user = result.user;
+        console.log(user);
+        setUser(user);
+      })
+      .catch(error => {
+        console.log('error', error);
+      })
+  }
+  const provider = new GoogleAuthProvider();
   return (
-    <div>
-      <p>This is log in page</p>
+    <div className='flex justify-center'>
+      <button onClick={handleGoogleSignIn} className="btn btn-outline btn-secondary block">Sign in</button>
+      <button onClick={handleGoogleSignOut} className="btn btn-outline btn-primary ml-5 block">Sign Out</button>
+      {
+        user.email && <div className='text-2xl'>
+          <p>{user.displayName}</p>
+          <p>{user.email}</p>
+          <img src={user.photoURL} alt='..' />
+        </div>
+      }
     </div>
   );
 };
---
+++ src/component/LogIn/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDqQxsY6Re7uKqHnY8GelLZcBhiak5kYC8",
+  authDomain: "simple-firebase-authenti-3e254.firebaseapp.com",
+  projectId: "simple-firebase-authenti-3e254",
+  storageBucket: "simple-firebase-authenti-3e254.appspot.com",
+  messagingSenderId: "809405931780",
+  appId: "1:809405931780:web:7d1eec8e39a1540ce53051"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app
